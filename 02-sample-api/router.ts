30b3112a993fd757dd7c40a9925a92229b6e4ddc
Régis Corrêa Oda <regis@otk.com.br>
2020-06-09 07:56:00 -0300

    Add methods

--- 02-sample-api/router.ts
+++ 02-sample-api/router.ts
@@ -1,9 +1,22 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+import {
+  getUsers,
+  getUser,
+  addUser,
+  updateUser,
+  deleteUser,
+} from "./controllers/user-controller.ts";
 
 const router = new Router();
 
 router.get("/", (ctx) => {
-  ctx.response.body = "Hello Deno!";
+  ctx.response.body = "Hello Deno!!!";
 });
+
+router.get("/users", getUsers);
+router.get("/user/:id", getUser);
+router.post("/user", addUser);
+router.put("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
 
 export default router;
