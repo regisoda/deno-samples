f9196f22af7622700a2b4695e4348a62f9bf60ca
Régis Corrêa Oda <regis@otk.com.br>
2020-06-11 08:07:41 -0300

    tests ok

--- 03-jwt-auth-api/routers.ts
+++ 03-jwt-auth-api/routers.ts
@@ -12,7 +12,7 @@ const header: Jose = {
 
 const router = new Router();
 
-export const login = async (ctx: Context) => {
+const login = async (ctx: Context) => {
     const { value } = await ctx.request.body();
     for (const user of users) {
         if (value.username === user.username && value.password === user.password) {
@@ -21,7 +21,6 @@ export const login = async (ctx: Context) => {
                 exp: setExpiration(new Date().getTime() + 60000),
             }
 
-            // Create JWT and send it to user
             const jwt = makeJwt({ key, header, payload });
             if (jwt) {
                 ctx.response.status = 200;
@@ -46,11 +45,11 @@ export const login = async (ctx: Context) => {
     };
 };
 
-export const guest = (ctx: Context) => {
+const guest = (ctx: Context) => {
     ctx.response.body = 'Guest success';
 };
 
-export const auth = (ctx: Context) => {
+const auth = (ctx: Context) => {
     ctx.response.body = 'Auth success';
 }
 
@@ -61,7 +60,7 @@ router.get("/", (ctx: Context) => {
 router
     .post('/login', login)
     .get('/guest', guest)
-    .get('/auth', authMiddleware, auth) // Registering authMiddleware for /auth endpoint only
+    .get('/auth', authMiddleware, auth)
     ;
 
 export default router;
