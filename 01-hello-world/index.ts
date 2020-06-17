5b233599d4f1f5e3d5b85347c4b686482f776a2c
OTK Sistemas <otk@OTKs-MacBook-Pro.local>
2020-06-17 06:11:17 -0300

    adjusts

--- 01-hello-world/index.ts
+++ 01-hello-world/index.ts
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.55.0/http/server.ts";
 const s = serve({ port: 8000 });
+console.log('Welcome to Deno 🦕')
 console.log("http://localhost:8000/");
 for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+  req.respond({ body: "\nHello World.\n" });
 }
